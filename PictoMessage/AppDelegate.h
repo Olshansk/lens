d213e09ad68788506d5d3f558b62930df0146aed
Daniel Olshansky <olshansky.daniel@gmail.com>
2013-05-27 12:45:05 -0700

    Initial Commit

---
+++ PictoMessage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PictoMessage
+//
+//  Created by Daniel Olshansky on 2013-05-27.
+//  Copyright (c) 2013 Daniel Olshansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PictoMessage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PictoMessage
+//
+//  Created by Daniel Olshansky on 2013-05-27.
+//  Copyright (c) 2013 Daniel Olshansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
